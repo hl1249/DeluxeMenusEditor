277c9dcdc6d9f708f13ed10f18fc03894550e0e9
hr1249412130 <1249412130@qq.com>
2025-04-03 04:01:29 +0800

    +update base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base:'./dist/',
+	base:'./DeluxeMenusEditor/',
   plugins: [vue(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
